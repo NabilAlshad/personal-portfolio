107b96e67511bdf8492f32ed49c1f112fcceea19
Nabil Alshad <57296992+NabilAlshad@users.noreply.github.com>
2021-06-06 19:18:52 +0600

    personal potfolio

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// navbar color change
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  navs: false,
+  autoplayTimeout: 8000,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+      margin: 20,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    800: {
+      items: 2,
+      nav: false,
+
+      loop: true,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+      loop: true,
+    },
+  },
+});
+s;
